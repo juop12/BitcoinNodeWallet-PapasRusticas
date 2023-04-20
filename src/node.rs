d5de43b7fb07bd4283a5f47feba7b1fb1d620c3b
Higlak <jaschieri@fi.uba.ar>
2023-04-20 13:43:10 -0300

    cargo fmt

--- src/node.rs
+++ src/node.rs
@@ -4,12 +4,12 @@ const DNS_PORT: u16 = 53; //DNS PORT
 
 /// Struct that represents the bitcoin node
 struct Node {
-    version : i32,
+    version: i32,
 }
 
 impl Node {
     pub fn new() -> Node {
-        Node {version:70015}
+        Node { version: 70015 }
     }
     /// Receives a dns address as a String and returns a Vector that contains all the addresses
     /// returned by the dns. If an error occured (for example, the dns address is not valid), it
@@ -27,10 +27,8 @@ impl Node {
         }
         socket_address_vector
     }
-
-    
 }
-/* 
+/*
  fn connect_user_to_server(dns: String) {
      let addrs_iter = (dns, DNS_PORT).to_socket_addrs().unwrap();
 
