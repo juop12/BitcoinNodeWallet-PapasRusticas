08b142cc32df4041d8231015680f081d97216aaa
Marco Natalini <mnatalini@fi.uba.ar>
2023-05-04 23:51:05 -0300

    primera version de config terminada

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 use std::{io::{BufRead, BufReader}, fs::File};
 
-const PARAMETER_AMOUNT : usize = 6;
-const CURRENT_VERSION : i32 = 70015;
+const CONFIG_FILENAME : &str = "nodo.conf";
+const PARAMETER_AMOUNT : usize = 5;
+//const CURRENT_VERSION : i32 = 70015;
+
 /*
 // nodo.conf
 
@@ -9,101 +11,106 @@ const CURRENT_VERSION : i32 = 70015;
     dns_port=53
     local_host=127,0,0,1
     local_port=1001
-    message_header_size=24
     log_file_path=path/to/log_file
 */
 
 #[derive(Debug)]
-enum ConfigError{
+pub enum ConfigError{
     ErrorReadingFile,
     ErrorFillingAttributes,
+    ErrorMismatchedFileName,
+    ErrorMismatchedQuantityOfParameters,
     ErrorMismatchedParameters,
 }
 
 #[derive(Debug)]
-struct Config {
+pub struct Config {
     version: i32,
     dns_port: u16,
     local_host: [u8; 4],
     local_port: u16,
-    message_header_size: usize,
     log_file_path: String,
 }
 
 impl Config{
-    fn validate_parameters(lines: &Vec<String>) -> Result<(), ConfigError>{
-    if lines.len() != PARAMETER_AMOUNT {
-        return Err(ConfigError::ErrorMismatchedParameters);        
-    }
-    match lines[0].parse::<i32>().ok() {
-        Some(version) => {
+    fn _validate_parameters(config_fields: &Vec<String>) -> Result<(), ConfigError>{
+        if config_fields.len() != PARAMETER_AMOUNT {
+            return Err(ConfigError::ErrorMismatchedQuantityOfParameters);        
+        }
+        
+        /*  
+        if let Some(version) = config_fields[0].parse::<i32>().ok(){
             if version != CURRENT_VERSION {
                 return Err(ConfigError::ErrorMismatchedParameters);
             }
-        },
-        None => return Err(ConfigError::ErrorMismatchedParameters),
-    };
-    Ok(())
+        }
+        */
+
+        // match config_fields[0].parse::<i32>().ok() {
+        //     Some(version) => {
+        //         if version != CURRENT_VERSION {
+        //             return Err(ConfigError::ErrorMismatchedParameters);
+        //         }
+        //     },
+        //     None => return Err(ConfigError::ErrorMismatchedParameters),
+        // };
+        
+        Ok(())
     }
     
-    fn initialize(lines: &Vec<String>)->Option<Config>{
+    fn _initialize(config_fields: &Vec<String>)->Option<Config>{
         let mut local_host: [u8; 4] = [0; 4];
+        let splitter = (*config_fields)[2].split(',');
         
-        //lines[2].split(',').map(f)
-
-        let mut j = 0;
-        for i in (*lines)[2].split(','){
-            local_host[j] = i.parse::<u8>().ok()?;
-            j += 1;
-        };
+        for (i, number) in (0_usize..).zip(splitter){
+            local_host[i] = number.parse::<u8>().ok()?;
+        }
         
 
         Some(Config {
-            version: lines[0].parse::<i32>().ok()?,
-            dns_port: lines[1].parse::<u16>().ok()?,
+            version: config_fields[0].parse::<i32>().ok()?,
+            dns_port: config_fields[1].parse::<u16>().ok()?,
             local_host, 
-            local_port: lines[3].parse::<u16>().ok()? ,
-            message_header_size: lines[4].parse::<usize>().ok()?,
-            log_file_path: lines[5].to_string(),
+            local_port: config_fields[3].parse::<u16>().ok()? ,
+            log_file_path: config_fields[4].to_string(),
         })
     }
 
-    fn from(lines: Vec<String>) -> Result<Config, ConfigError>{
+    fn _from(config_fields: Vec<String>) -> Result<Config, ConfigError>{
 
-        Config::validate_parameters(&lines)?;
-        let config = Config::initialize(&lines);
+        Config::_validate_parameters(&config_fields)?;
 
-        match config {
+        match Config::_initialize(&config_fields) {
             Some(config) => Ok(config),
             None => Err(ConfigError::ErrorFillingAttributes),
         }
     }
 
     pub fn from_path(path: &str) -> Result<Config, ConfigError>{
-        let file = open_config_handler(path)?;
+        if !path.ends_with(CONFIG_FILENAME){
+            return Err(ConfigError::ErrorMismatchedFileName)
+        }
+
+        let file = _open_config_handler(path)?;
         let reader: BufReader<File> = BufReader::new(file);
-        
-        let mut fields = Vec::new();
+
+        let mut config_fields = Vec::new();
                 
-        for read_line in reader.lines() {
-            match read_line {
-                Ok(line) => {
-                    let mut splitter = line.splitn(2, '='); 
-                    if let Some(second_half) = splitter.nth(1) {
-                        fields.push(second_half.to_string()); // Crear una copia de la segunda mitad de la línea
-                    } else {
-                        return Err(ConfigError::ErrorReadingFile);
-                    }
+        for line in reader.lines() {
+            match line {
+                Ok(field) => {
+                    let splitted_line: Vec<&str> = field.split('=').collect();
+                    config_fields.push(splitted_line[1].to_string());
                 },
                 Err(_) => return Err(ConfigError::ErrorReadingFile),
             }
         }
         
-        Config::from(fields)
+        Config::_from(config_fields)
     }
 }
 
-fn open_config_handler(path: &str) -> Result<File, ConfigError> {
+fn _open_config_handler(path: &str) -> Result<File, ConfigError> {
     match File::open(path){
         Ok(file)=> Ok(file),
         Err(_) => Err(ConfigError::ErrorReadingFile),
@@ -113,55 +120,59 @@ fn open_config_handler(path: &str) -> Result<File, ConfigError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
-    fn test_1_valid_parameters_creates_valid_config(){
-        let parameters = vec![
-            "70015".to_string(),
-            "53".to_string(),
-            "127,0,0,1".to_string(),
-            "1001".to_string(),
-            "24".to_string(),
-            "path/to/log_file".to_string(),
-        ];
-        assert!(Config::from(parameters).is_ok());
+    fn test_config_1_valid_file_creates_config(){
+        assert!(Config::from_path("src/nodo.conf").is_ok());
     }
 
     #[test]
-    fn test_2_invalid_ammount_parameters_cannot_create_config(){
-        let parameters = vec![
-            "70015".to_string(),
-            "53".to_string(),
-            "127,0,0,1".to_string(),
-            "1001".to_string(),
-            "24".to_string(),
-        ];
-        assert!(Config::from(parameters).is_err());
-    }
-
-    #[test]
-    fn test_3_invalid_type_of_parameters_cannot_create_config(){
-        let parameters = vec![
-            "70015".to_string(),
-            "53".to_string(),
-            "34,92,33,1".to_string(),
-            "2023".to_string(),
-            "path/to/log_file".to_string(),
-            "24".to_string(),
-        ];
-        assert!(Config::from(parameters).is_err());
-    }
-
-
-    #[test]
-    fn test_config_4_invalid_file_cannot_create_config(){
+    fn test_config_2_invalid_file_cannot_create_config(){
         assert!(Config::from_path("invalid_file.conf").is_err());
+    } 
+
+    #[test]
+    fn test_config_3_saves_parameters_correctly(){
+        let parameters = vec![
+            "70015".to_string(),
+            "53".to_string(),
+            "127,0,0,1".to_string(),
+            "1001".to_string(),
+            "path/to/log_file".to_string(),
+        ];
+        
+        let config = Config::_from(parameters).expect("Could not create config from valid parameters.");
+        
+        assert_eq!(config.version, 70015);
+        assert_eq!(config.dns_port, 53);
+        assert_eq!(config.local_host, [127,0,0,1]);
+        assert_eq!(config.local_port, 1001);
+        assert_eq!(config.log_file_path, "path/to/log_file".to_string());
     }
 
     #[test]
-    fn test_config_5_valid_file_creates_config(){
-        assert!(Config::from_path("nodo.conf").is_ok());
+    fn test_config_4_invalid_ammount_parameters_cannot_create_config(){
+        let parameters = vec![
+            "70015".to_string(),
+            "53".to_string(),
+            "127,0,0,1".to_string(),
+            "1001".to_string(),
+        ];
+        
+        assert!(Config::_from(parameters).is_err());
     }
-
+    
+    #[test]
+    fn test_config_5_invalid_type_of_parameters_cannot_create_config(){
+        let parameters = vec![
+            "70015".to_string(),
+            "53".to_string(),
+            "34".to_string(),
+            "this should be a u16".to_string(),
+            "path/to/log_file".to_string(),
+        ];
+        
+        assert!(Config::_from(parameters).is_err());
+    }        
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 
 
 fn main() {
-    /*
+    use proyecto::config::Config;
+    use std::env;
+
     let path: Vec<String> = env::args().collect();
-    if !path.contains("nodo.conf") {
+    if !path.contains(&String::from("nodo.conf")) {
         return; //error
     }
 
-    let config: Config = read_config_file(path);
-    */
+    let config: Config = match(Config::from_path("nodo.conf")){
+        Ok(config) => config,
+        Err(_) => return, //error
+    };
+    
 }
 
 
