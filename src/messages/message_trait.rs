94d7ae7428022d71ed3c574b2fb6b5cfc3005c78
rovifran <89984911+rovifran@users.noreply.github.com>
2023-05-21 13:23:12 -0300

    Fixeo de documentacion en trait message para que se vea bien en el ppt

--- src/messages/message_trait.rs
+++ src/messages/message_trait.rs
@@ -11,7 +11,8 @@ pub trait Message {
     /// Transforms the message to bytes, usig the p2p bitcoin protocol
     fn to_bytes(&self) -> Vec<u8>;
 
-    /// Creates the coresponding message, using a slice of bytes, wich must be of the correct size, otherwise an error will be returned.
+    /// Creates the coresponding message, using a slice of bytes, wich must be
+    /// of the correct size, otherwise an error will be returned.
     fn from_bytes(slice: &[u8]) -> Result<Self::MessageType, MessageError>;
 
     /// Gets the header message corresponding to the corresponding message
